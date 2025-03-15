729e5478a6cbb621389453b527803281474ec57e
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2025-03-15 23:11:20 +0530

    Move Backend outside

--- Backend/models/coupon.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const couponSchema = new mongoose.Schema({
-  code: { type: String, required: true, unique: true },
-  enabled: { type: Boolean, default: true },
-});
-
-module.exports = mongoose.model("Coupon", couponSchema);
--- Backend/routes/couponRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { claimCoupon} = require("../controllers/couponController");
-
-const router = express.Router();
-
-// Route to claim a coupon
-router.post("/claim", claimCoupon);
-
-module.exports = router;
