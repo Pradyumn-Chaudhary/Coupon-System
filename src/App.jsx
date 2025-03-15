3a0c7041387c6e3aa9483c30fc5ee99aa69b741e
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2025-03-15 18:08:49 +0530

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import ClaimCoupon from "./pages/ClaimCoupon";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<ClaimCoupon />} /> {/* User Claim Page */}
+        <Route path="/login" element={<AdminLogin />} /> {/* Admin Login */}
+        <Route path="/dashboard" element={<AdminDashboard />} /> {/* Admin Panel */}
+        <Route path="/claim-history" element={<ClaimHistory />} /> {/* View Claim History */}
+        <Route path="*" element={<NotFound />} /> {/* 404 Page */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/ClaimCoupon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClaimCoupon = () => {
+  return (
+    <div>
+      Claim Coupen
+    </div>
+  )
+}
+
+export default ClaimCoupon
